011980442cf055cfc105a0f3a54e50027e45e18e
Pedro <pedroestudos20252025@gmail.com>
2026-05-18 15:22:49 -0300

    feat(Auth): Add routes RestFull for login, register and refresh

---
+++ src/main/java/org/technew/linkedin2/controllers/auth/AuthController.java
@@ -0,0 +1,34 @@
+package org.technew.linkedin2.controllers.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.technew.linkedin2.model.entities.user.auth.RefreshToken;
+import org.technew.linkedin2.services.security.AuthService;
+import org.technew.linkedin2.services.security.DTOs.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthLoginDTO authLoginDTO){
+        return ResponseEntity.ok(authService.login(authLoginDTO));
+    }
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterDTO registerDTO){
+        authService.register(registerDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponseDTO> refresh(@RequestBody RefreshTokenDTO refreshTokenDTO){
+        return ResponseEntity.ok(authService.refresh(refreshTokenDTO));
+    }
+}
